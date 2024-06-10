c3d651a4cc0b55c35f6340b285d672c8090a1133
ouvic <ouvic215@gmail.com>
2024-06-10 23:02:03 +0800

    Update image_text_gen.py

--- image_text_gen.py
+++ image_text_gen.py
@@ -7,6 +7,8 @@ from model.text_model import *
 from rouge_score import rouge_scorer
 from scripts.train_text_gen import *
 import torchvision.transforms as transforms
+from PIL import Image
+import pandas as pd
 
 def evaluate_score(gen_text, targets):
     scorer = rouge_scorer.RougeScorer(['rouge1', 'rouge2', 'rougeL'], use_stemmer=True)
@@ -14,63 +16,73 @@ def evaluate_score(gen_text, targets):
     for key in scores:
         print(f'{key}: {scores[key]}')
         
-def valid_rouge_score(model, df):
+def valid_rouge_score(model, df, image_directory):
     model.eval()
     transform = transforms.Compose([
-        # if using PIL to load image, comment this line
-        transforms.ToPILImage(),
         transforms.Resize(256),
         transforms.CenterCrop(224),
         transforms.ToTensor(),
         transforms.Normalize([0.485, 0.456, 0.406], [0.229, 0.224, 0.225]),
         transforms.Grayscale(num_output_channels=1),
     ])
-    # just load all valid img & report
-    for data in df:
-        img = transform(data['image']).to(config.device)
+    for index, row in df.iterrows():
+        image_path = f"{image_directory}/{row['name']}_1_1.png"
+        img = Image.open(image_path).convert('RGB')
+        img = transform(img).unsqueeze(0).to(config.device)
         report = model.generate(img)
-        evaluate_score(report, data['text'])
+        evaluate_score(report, row['text'])
 
 epochs = 1000
 
-def train_image_text(model, optim, train_df, valid_df):
-    
+def train_image_text(model, optim, train_df, valid_df, train_image_dir, valid_image_dir):
     model.to(config.device)
     model.train()
     
-    for iter in range(epochs):
+    transform = transforms.Compose([
+        transforms.Resize(256),
+        transforms.CenterCrop(224),
+        transforms.ToTensor(),
+        transforms.Normalize([0.485, 0.456, 0.406], [0.229, 0.224, 0.225]),
+        transforms.Grayscale(num_output_channels=1),
+    ])
     
-        for batch in train_df:
-            img, targets = batch['image'].to(config.device), batch['text'].to(config.device)
+    for iter in range(epochs):
+        for index, row in train_df.iterrows():
+            image_path = f"{train_image_dir}/{row['name']}_1_1.png"
+            img = Image.open(image_path).convert('RGB')
+            img = transform(img).unsqueeze(0).to(config.device)
+            targets = row['text']
             logits, loss = model(img, targets)
             optim.zero_grad(set_to_none=True)
             loss.backward()
             optim.step()
-        # break                           # for testing
         
         if iter % 100 == 0:
             print(f'{iter} / {epochs} training loss : {loss}')
-            valid_rouge_score(model, valid_df)
+            valid_rouge_score(model, valid_df, valid_image_dir)
         
         if iter % 200 == 0 and iter > 0:
             torch.save(model.state_dict(), f'image_gen_report_for_{iter}_iters.pt')
         
     model.eval()
-    valid_rouge_score(model, valid_df)
+    valid_rouge_score(model, valid_df, valid_image_dir)
     torch.save(model.state_dict(), f'text_gen_train_for_{epochs}_iters.pt')
 
+train_csv_path = './dataset/merged_reports_train.csv'
+valid_csv_path = './dataset/merged_reports_valid.csv'
+
+train_df = pd.read_csv(train_csv_path)
+valid_df = pd.read_csv(valid_csv_path)
+
+train_image_dir = './CXIRG_Data/train_data/images'
+valid_image_dir = './CXIRG_Data/valid_data/images'
 
 encoder = Encoder(config).to(config.device)
-load_checkpoint(encoder, 'your checkpoint')
+load_checkpoint(encoder, './encoder.pt')
 
 decoder = Decoder(config).to(config.device)
-load_checkpoint(decoder, 'your checkpoint')
+load_checkpoint(decoder, './decoder.pt')
 model = EncoderDecoderModel(encoder=encoder, decoder=decoder, config=config).to(config.device)
 optim = torch.optim.AdamW(model.parameters(), lr=config.lr)
 
-'''
-train_df = {}
-valid_df = {}
-
-train_image_text(model, optim, train_df, valid_df)
-'''
+train_image_text(model, optim, train_df, valid_df, train_image_dir, valid_image_dir)
